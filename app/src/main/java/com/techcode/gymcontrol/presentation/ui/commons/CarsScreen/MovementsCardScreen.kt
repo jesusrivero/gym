a8f4f7e2d22784f91ee82edb72adb91fed94ead2
JesusR <jesusparrauen@gmail.com>
2025-05-14 00:34:02 -0700

    Aparrtado de ultimos movimientos modificado

--- app/src/main/java/com/techcode/gymcontrol/presentation/ui/commons/CarsScreen/MovementsCardScreen.kt
+++ app/src/main/java/com/techcode/gymcontrol/presentation/ui/commons/CarsScreen/MovementsCardScreen.kt
@@ -1,12 +1,13 @@
 package com.techcode.gymcontrol.presentation.ui.commons.CarsScreen
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
@@ -14,104 +15,323 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun MovementsCarScreen() {
-	Column(
-		modifier = Modifier
-			.fillMaxSize()
-			.padding(16.dp)
-	) {
-		
-		Text(
-			text = "Últimos Movimientos",
-			style = MaterialTheme.typography.headlineSmall.copy(
-				fontWeight = FontWeight.Bold
-			),
-			modifier = Modifier.padding(bottom = 16.dp)
-		)
 
-		getSampleMovimientos().forEach{ item ->
-			MovimientoItem(movimiento = item)
-		}
-	}
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+    ) {
+
+        Text(
+            text = "Últimos Movimientos",
+            style = MaterialTheme.typography.headlineSmall.copy(
+                fontWeight = FontWeight.Bold
+            ),
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+        MovementsCard(
+            color = Color(0xFF4CAF50),
+            modifier = Modifier
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Column(
+                    modifier = Modifier.weight(1f)
+                ) {
+
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Text(
+                            text = "$",
+                            style = MaterialTheme.typography.bodyLarge.copy(
+                                fontWeight = FontWeight.Bold,
+                                color = MaterialTheme.colorScheme.primary
+                            ),
+                            modifier = Modifier.padding(end = 8.dp)
+                        )
+                        Text(
+                            text = "Pago de jesus",
+                            style = MaterialTheme.typography.bodyLarge.copy(
+                                fontWeight = FontWeight.SemiBold
+                            )
+                        )
+                    }
+
+
+                    Text(
+                        text = "21/12/12",
+                        style = MaterialTheme.typography.bodySmall,
+                        color = MaterialTheme.colorScheme.onSurfaceVariant,
+                        modifier = Modifier.padding(top = 4.dp)
+                    )
+                }
+
+
+                Text(
+                    text = "15$",
+                    style = MaterialTheme.typography.bodyLarge.copy(
+                        fontWeight = FontWeight.Bold,
+                        color = MaterialTheme.colorScheme.primary
+                    )
+                )
+            }
+        }
+        Spacer(modifier = Modifier.height(6.dp))
+        MovementsCard(
+            color = Color(0xFF4CAF50),
+            modifier = Modifier
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Column(
+                    modifier = Modifier.weight(1f)
+                ) {
+
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Text(
+                            text = "$",
+                            style = MaterialTheme.typography.bodyLarge.copy(
+                                fontWeight = FontWeight.Bold,
+                                color = MaterialTheme.colorScheme.primary
+                            ),
+                            modifier = Modifier.padding(end = 8.dp)
+                        )
+                        Text(
+                            text = "Pago de jesus",
+                            style = MaterialTheme.typography.bodyLarge.copy(
+                                fontWeight = FontWeight.SemiBold
+                            )
+                        )
+                    }
+
+
+                    Text(
+                        text = "21/12/12",
+                        style = MaterialTheme.typography.bodySmall,
+                        color = MaterialTheme.colorScheme.onSurfaceVariant,
+                        modifier = Modifier.padding(top = 4.dp)
+                    )
+                }
+
+
+                Text(
+                    text = "15$",
+                    style = MaterialTheme.typography.bodyLarge.copy(
+                        fontWeight = FontWeight.Bold,
+                        color = MaterialTheme.colorScheme.primary
+                    )
+                )
+            }
+        }
+        Spacer(modifier = Modifier.height(6.dp))
+        MovementsCard(
+            color = Color(0xFF4CAF50),
+            modifier = Modifier
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Column(
+                    modifier = Modifier.weight(1f)
+                ) {
+
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Text(
+                            text = "$",
+                            style = MaterialTheme.typography.bodyLarge.copy(
+                                fontWeight = FontWeight.Bold,
+                                color = MaterialTheme.colorScheme.primary
+                            ),
+                            modifier = Modifier.padding(end = 8.dp)
+                        )
+                        Text(
+                            text = "Pago de jesus",
+                            style = MaterialTheme.typography.bodyLarge.copy(
+                                fontWeight = FontWeight.SemiBold
+                            )
+                        )
+                    }
+
+
+                    Text(
+                        text = "21/12/12",
+                        style = MaterialTheme.typography.bodySmall,
+                        color = MaterialTheme.colorScheme.onSurfaceVariant,
+                        modifier = Modifier.padding(top = 4.dp)
+                    )
+                }
+
+
+                Text(
+                    text = "15$",
+                    style = MaterialTheme.typography.bodyLarge.copy(
+                        fontWeight = FontWeight.Bold,
+                        color = MaterialTheme.colorScheme.primary
+                    )
+                )
+            }
+        }
+        Spacer(modifier = Modifier.height(6.dp))
+        MovementsCard(
+            color = Color(0xFF4CAF50),
+            modifier = Modifier
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Column(
+                    modifier = Modifier.weight(1f)
+                ) {
+
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Text(
+                            text = "$",
+                            style = MaterialTheme.typography.bodyLarge.copy(
+                                fontWeight = FontWeight.Bold,
+                                color = MaterialTheme.colorScheme.primary
+                            ),
+                            modifier = Modifier.padding(end = 8.dp)
+                        )
+                        Text(
+                            text = "Pago de jesus",
+                            style = MaterialTheme.typography.bodyLarge.copy(
+                                fontWeight = FontWeight.SemiBold
+                            )
+                        )
+                    }
+
+
+                    Text(
+                        text = "21/12/12",
+                        style = MaterialTheme.typography.bodySmall,
+                        color = MaterialTheme.colorScheme.onSurfaceVariant,
+                        modifier = Modifier.padding(top = 4.dp)
+                    )
+                }
+
+
+                Text(
+                    text = "15$",
+                    style = MaterialTheme.typography.bodyLarge.copy(
+                        fontWeight = FontWeight.Bold,
+                        color = MaterialTheme.colorScheme.primary
+                    )
+                )
+            }
+        }
+        Spacer(modifier = Modifier.height(6.dp))
+        MovementsCard(
+            color = Color(0xFF4CAF50),
+            modifier = Modifier
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Column(
+                    modifier = Modifier.weight(1f)
+                ) {
+
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Text(
+                            text = "$",
+                            style = MaterialTheme.typography.bodyLarge.copy(
+                                fontWeight = FontWeight.Bold,
+                                color = MaterialTheme.colorScheme.primary
+                            ),
+                            modifier = Modifier.padding(end = 8.dp)
+                        )
+                        Text(
+                            text = "Pago de jesus",
+                            style = MaterialTheme.typography.bodyLarge.copy(
+                                fontWeight = FontWeight.SemiBold
+                            )
+                        )
+                    }
+
+
+                    Text(
+                        text = "21/12/12",
+                        style = MaterialTheme.typography.bodySmall,
+                        color = MaterialTheme.colorScheme.onSurfaceVariant,
+                        modifier = Modifier.padding(top = 4.dp)
+                    )
+                }
+
+
+                Text(
+                    text = "15$",
+                    style = MaterialTheme.typography.bodyLarge.copy(
+                        fontWeight = FontWeight.Bold,
+                        color = MaterialTheme.colorScheme.primary
+                    )
+                )
+            }
+        }
+    }
+
 }
+
 
 @Composable
-fun MovimientoItem(movimiento: Movimiento) {
-	Card(
-		modifier = Modifier.fillMaxWidth(),
-		colors = CardDefaults.cardColors(
-			containerColor = MaterialTheme.colorScheme.surfaceVariant,
-		),
-		elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
-	) {
-		Row(
-			modifier = Modifier
-				.fillMaxWidth()
-				.padding(16.dp),
-			horizontalArrangement = Arrangement.SpaceBetween,
-			verticalAlignment = Alignment.CenterVertically
-		) {
-			Column(
-				modifier = Modifier.weight(1f)
-			) {
-				
-				Row(
-					verticalAlignment = Alignment.CenterVertically
-				) {
-					Text(
-						text = "$",
-						style = MaterialTheme.typography.bodyLarge.copy(
-							fontWeight = FontWeight.Bold,
-							color = MaterialTheme.colorScheme.primary
-						),
-						modifier = Modifier.padding(end = 8.dp)
-					)
-					Text(
-						text = movimiento.descripcion,
-						style = MaterialTheme.typography.bodyLarge.copy(
-							fontWeight = FontWeight.SemiBold
-						)
-					)
-				}
-				
-				
-				Text(
-					text = movimiento.fecha,
-					style = MaterialTheme.typography.bodySmall,
-					color = MaterialTheme.colorScheme.onSurfaceVariant,
-					modifier = Modifier.padding(top = 4.dp)
-				)
-			}
-			
-			
-			Text(
-				text = movimiento.monto,
-				style = MaterialTheme.typography.bodyLarge.copy(
-					fontWeight = FontWeight.Bold,
-					color = MaterialTheme.colorScheme.primary
-				)
-			)
-		}
-	}
-}
+fun MovementsCard(
+    color: Color,
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit,
+) {
+
+    Card(
+        modifier = modifier.fillMaxWidth(),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surfaceVariant,
+        ),
+        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+
+            )
+
+            content()
+        }
+    }
 
 
-data class Movimiento(
-	val descripcion: String,
-	val fecha: String,
-	val monto: String
-)
-
-fun getSampleMovimientos(): List<Movimiento> {
-	return listOf(
-		Movimiento("Pago de Jesus Rivero", "05/06/2025", "15$"),
-		Movimiento("Inscripcion Nueva - P1", "03/01/2025", "4$"),
-		Movimiento("Inscripción nueva - P3", "05/05/2025", "4$"),
-		Movimiento("Pago de cuota mensual", "04/05/2025", "15$"),
-		Movimiento("Renovación membresía", "03/05/2025", "15$")
-	)
-}
