2d6f03a8300ac61ee40ae73051f9e922f5c7dcae
JesusR <jesusparrauen@gmail.com>
2025-06-01 14:31:09 -0400

    Ajustes en notificaciones, creacion del dataclass NotificationSttings.kt

--- app/src/main/java/com/techcode/gymcontrol/data/sharedPreferences/PreferencesManager.kt
+++ app/src/main/java/com/techcode/gymcontrol/data/sharedPreferences/PreferencesManager.kt
@@ -27,6 +27,7 @@ class PreferencesManager(context: Context) {
 		const val KEY_PUSH_NOTIFICATIONS = "push_notifications"
 	}
 
+
 	private val sharedPreferences: SharedPreferences =
 		context.getSharedPreferences("GymControlPreferences", Context.MODE_PRIVATE)
 	private val notificationPrefs: SharedPreferences =
