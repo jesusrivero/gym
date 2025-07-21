0619c3842106edae4cdeaf7b0db80f6854da8ecc
Jesus Morales <21moralesjesus@gmail.com>
2025-07-21 15:02:43 -0400

    Fix: Add missing comma in ClienteScreen
    This commit adds a missing comma in the `ClienteScreen.kt` file.

--- app/src/main/java/com/jesus/gymcontrol/presentation/ui/settings/details/ClienteScreen.kt
+++ app/src/main/java/com/jesus/gymcontrol/presentation/ui/settings/details/ClienteScreen.kt
@@ -51,6 +51,8 @@ fun ClientMainScreen(
             style = MaterialTheme.typography.headlineSmall,
             fontWeight = FontWeight.Bold
         )
+	    
+	    
 	    Text(
 		    text = "Aqui podrás ver tu progreso",
 		    style = MaterialTheme.typography.headlineSmall,
